6be2a97db9e867660707c4900aa99d80216138a4
now8018-droid <now8018@gmail.com>
2026-03-23 15:59:40 +0700

    Delete R-Core directory

--- R-Core/module/SaveHealth/client/client.lua
+++
@@ -1,7 +0,0 @@
-ESX = exports["es_extended"]:getSharedObject()
-
-RegisterNetEvent("LUV.CORE:SAVEHEALTH:SETHEALTH")
-AddEventHandler("LUV.CORE:SAVEHEALTH:SETHEALTH", function(ARRAY)
-    SetEntityHealth(PlayerPedId(), ARRAY.health)
-    SetPedArmour(PlayerPedId(), ARRAY.armour)
-end)
